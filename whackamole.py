73b66007e4a3ba7c8b3b989a4bed450ce2ce1212
Ameer <a.latif1@ufl.edu>
2024-11-17 17:56:22 -0500

    Redefined the mole's position as variables to be used for the event.type check. Defined the x and y coordinates of the click event so they could bestored. Modified the event.type check to randomly adjust the mole's position based on if the click falls within the 32x32 constraints.

--- whackamole.py
+++ whackamole.py
@@ -1,4 +1,7 @@
+from random import randrange
+
 import pygame
+import random
 
 
 def main():
@@ -9,19 +12,25 @@ def main():
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
+        mole_position_x = 0
+        mole_position_y = 0
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    print(event.pos)
+                    click_x = event.pos[0]
+                    click_y = event.pos[1]
+                    if (click_x > mole_position_x and click_x < mole_position_x + 32) and (click_y > mole_position_y and click_y < mole_position_y + 32):
+                        mole_position_x = random.randrange(0, 20) * 32
+                        mole_position_y = random.randrange(0, 16) * 32
             screen.fill((255, 253, 208))
             for i in range (32):
                 pygame.draw.line(screen, "black", (32 * i, 0), (32 * i, 512))
             for i in range (32):
                 pygame.draw.line(screen, "black", (0, 32 * i), (640, 32 * i))
-            screen.blit(mole_image, mole_image.get_rect(topleft=(0, 0)))
+            screen.blit(mole_image, mole_image.get_rect(topleft=(mole_position_x, mole_position_y)))
             pygame.display.flip()
             clock.tick(60)
     finally:
